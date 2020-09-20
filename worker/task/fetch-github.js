99fdf98700004c42923315d2c0f5cc5258a4e7ac
Tom <tomaszsobkowicz21@gmail.com>
2020-09-20 19:10:14 +0200

    Add redis, create basic filter algorithm

--- worker/task/fetch-github.js
+++ worker/task/fetch-github.js
@@ -1,12 +1,21 @@
 const fetch = require('node-fetch');
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+const setAsync = promisify(client.set).bind(client);
 
 const baseUrl = 'https://jobs.github.com/positions.json';
 
 async function fetchGithub() {
+
+  console.log('fetching github jobs api');
+
   let resultCount = 1,
       onPage = 0;
   const allJobs = [];
 
+  // fetch all pages
   while (resultCount > 0) {
     const res = await fetch(`${baseUrl}?page=${onPage}`);
     const jobs = await res.json();
@@ -14,9 +23,31 @@ async function fetchGithub() {
     resultCount = jobs.length;
     console.log('got', resultCount, 'jobs');
     onPage++;
-  }
+  };
 
   console.log('got', allJobs.length, 'jobs');
+
+  // basic filter algorithm
+  const jrJobs = allJobs.filter(job => {
+    const jobTitle = job.title.toLowerCase();
+
+    // algo logic
+    if (
+      jobTitle.includes('senior') ||
+      jobTitle.includes('manager') ||
+      jobTitle.includes('sr.') ||
+      jobTitle.includes('architect')
+    ) {
+      return false
+    }
+
+    return true;
+  });
+  console.log('filtered down to', jrJobs.length);
+
+  // set in redis
+  const success = await setAsync('github', JSON.stringify(jrJobs));
+  console.log({success});
 }
 
 module.exports = fetchGithub;
